675b64d32e7bf6526bc887aa38fb6ea476e7448f
Nahin Alam <nahin.alam@northsouth.edu>
2024-01-30 14:12:15 +0600

    Update settings.py

--- myApp/settings.py
+++ myApp/settings.py
@@ -17,14 +17,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
-
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
